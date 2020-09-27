d3c8d2707d7859ca27f03be8bb534649f685bbf0
Leo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2020-09-27 01:24:49 -0300

    Updates

---
+++ precache-manifest.be88f6b27989c940f7b6602b61e56726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ff5d1887328e70e8d0c67b044ab8ac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be478ee422ed8ea4a662",
+    "url": "/portfolio/static/js/2.12a518c2.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/portfolio/static/js/2.12a518c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f97cc8c13b2e2c1781",
+    "url": "/portfolio/static/js/main.4eaa6e86.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "875442f7b59ba535ea9a7ebaa11f638f",
+    "url": "/portfolio/static/media/lab-spotify-50.875442f7.md"
+  }
+]);
